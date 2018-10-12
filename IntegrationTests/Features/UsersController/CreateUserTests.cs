ef6a89dbad07c35a563137d1cd7c06a1066ca5d6
chrisevansbede <chris.evans@bedegaming.com>
2018-10-12 18:06:01 +0100

    Adds testing helpers / wrapper

--- IntegrationTests/Features/UsersController/CreateUserTests.cs
+++ IntegrationTests/Features/UsersController/CreateUserTests.cs
@@ -14,28 +14,15 @@ namespace IntegrationTests.Features.UsersController
         [Test]
         public async Task CreateUserReturns200WithUserModel()
         {
-            var requestBody = new User
-            {
-                Username = "TestUsername",
-                Password = "TestPassword",
-                Email = "email@email.com"
-            };
-
-            var jsonBody = JsonConvert.SerializeObject(requestBody);
-            var stringBody = new StringContent(jsonBody, Encoding.UTF8, "application/json");
-
-            var request = new HttpRequestMessage(HttpMethod.Post, "api/users")
-            {
-                Content = stringBody
-            };
-
-            var response = await Client.SendAsync(request);
-            var result = await response.Content.ReadAsStringAsync();
-            var resultUser = JsonConvert.DeserializeObject<User>(result);
+            var requestUser = TestHelpers.CreateUserWithRandomData();
+            var request = TestHelpers.CreatePostRequest("api/users", requestUser);
+            var (response, responseObject) = await SendRequest<User>(request);
 
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-            Assert.AreEqual("TestUsername", resultUser.Username);
-            Assert.Greater(resultUser.UserId, 0);
+            Assert.AreEqual(requestUser.Username, responseObject.Username);
+            Assert.AreEqual(requestUser.Password, responseObject.Password);
+            Assert.AreEqual(requestUser.Email, responseObject.Email);
+            Assert.Greater(responseObject.UserId, 0);
         }
     }
 }
---
+++ IntegrationTests/TestHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Domain.Models;
+using Newtonsoft.Json;
+
+namespace IntegrationTests
+{
+    public static class TestHelpers
+    {
+        #region Requests
+        
+        public static HttpRequestMessage CreatePostRequest(string url, object body)
+        {
+            var stringBody = new StringContent(
+                JsonConvert.SerializeObject(body),
+                Encoding.UTF8,
+                "application/json");
+            
+            return new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = stringBody
+            };
+        }
+
+        #endregion
+
+        #region DataGeneration
+
+        public static User CreateUserWithRandomData(
+            string username = null, string password = null, string email = null)
+        {
+            var user = new User
+            {
+                Username = username ?? Faker.NameFaker.FirstName(),
+                Password = password ?? Faker.StringFaker.AlphaNumeric(20),
+                Email = email ?? Faker.InternetFaker.Email()
+            };
+
+            return user;
+        }
+
+        #endregion
+    }
+}
